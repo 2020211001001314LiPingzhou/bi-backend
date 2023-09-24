5fae358d295081c02df08fbf886a665890cdbeaa
lpz <1441483666@qq.com>
2023-09-24 16:09:08 +0800

    Initial commit

---
+++ src/main/java/com/yupi/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 18720308580
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2023-09-24 14:41:20
+* @Entity com.yupi.springbootinit.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/com/yupi/springbootinit/service/ChartService.java
@@ -0,0 +1,13 @@
+package com.yupi.springbootinit.service;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 18720308580
+* @description 针对表【chart(图表信息表)】的数据库操作Service
+* @createDate 2023-09-24 14:41:20
+*/
+public interface ChartService extends IService<Chart> {
+
+}
